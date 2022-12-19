da13575ca18c0d5437d82bf963c906dfa1ca426c
Marina Zinchenko <amanda79116@gmail.com>
2022-12-18 22:46:51 -0600

    added options

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -23,7 +23,7 @@ class CallbackTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
