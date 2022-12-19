46a39be809e5090edfcac0e66f1658d76105bdb5
Marina Zinchenko <amanda79116@gmail.com>
2022-12-18 21:57:16 -0600

    positive test

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CallbackTest {
     private WebDriver driver;
@@ -22,9 +26,15 @@ class CallbackTest {
     }
 
     @Test
-    void shouldTestSomething() {
+    void positiveTest() throws InterruptedException {
         driver.get("http://localhost:9999");
-        System.out.println("a");
-        //throw new UnsupportedOperationException();
+        List<WebElement> input = driver.findElements(By.className("input__control"));
+        input.get(0).sendKeys("Марина Зинченко");
+        input.get(1).sendKeys("+79111234567");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("paragraph")).getText().trim();
+        assertEquals(expected, actual);
     }
 }
